cfd6259d5c2b979ce65920e67256eec03553f60a
treyhahn <treyhahn@treys-mbp-2.wireless.newschool.edu>
2015-10-23 10:52:22 -0400

    Initial Commit

---
+++ Talking Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Talking Calculator
+//
+//  Created by Trey Hahn on 10/23/15.
+//  Copyright © 2015 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
